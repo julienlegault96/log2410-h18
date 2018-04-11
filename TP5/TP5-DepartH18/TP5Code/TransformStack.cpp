9ca4e73cf7071cf73e0b906c2b0f63542d89116b
Christophe <christophe.bourque-bedard@polymtl.ca>
2018-04-11 09:14:55 -0400

    Modifications half-assed de TransformStack

--- TP5/TP5-DepartH18/TP5Code/TransformStack.cpp
+++ TP5/TP5-DepartH18/TP5Code/TransformStack.cpp
@@ -8,7 +8,6 @@ Objet3DTransform & TransformStack::getCurrent(void)
 	// Si la pile de transformation est vide faire
 	//    - Pousser une transformation vide sur la pile
 	// Retourner la derniere transformation de la pile
-
 	return Objet3DTransform();
 }
 
@@ -18,15 +17,25 @@ void TransformStack::pushCurrent(void)
 	//    - Pousser la dernière transformation sur la pile
 	// Sinon faire
 	//    - Pousser une transformation vide sur la pile
+	if (!m_transforms.empty())
+	{
+		m_transforms.push_back(std::unique_ptr<Objet3DTransform>(new Objet3DTransform(getCurrent())));
+	}
+	else
+	{
+		m_transforms.push_back(std::unique_ptr<Objet3DTransform>(new Objet3DTransform()));
+	}
 }
 
 void TransformStack::push(const Objet3DTransform & t)
 {
 	// Pousser la transformation recue en parametre sur la pile
+	m_transforms.push_back(std::unique_ptr<Objet3DTransform>(new Objet3DTransform(t)));
 }
 
 void TransformStack::pop(void)
 {
 	// Si la pile n'est pas vide faire
 	//    - Retirer la derniere transformation de sur la pile
+	m_transforms.pop_back();
 }
